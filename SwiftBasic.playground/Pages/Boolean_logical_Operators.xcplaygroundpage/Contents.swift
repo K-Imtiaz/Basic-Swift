6d5677804db5175d9ff8606d2d99daf612003088
k-imtiaz <shishir501103@gmail.com>
2025-03-23 00:54:11 +0600

    Updated boolea

--- SwiftBasic.playground/Pages/Boolean_logical_Operators.xcplaygroundpage/Contents.swift
+++ SwiftBasic.playground/Pages/Boolean_logical_Operators.xcplaygroundpage/Contents.swift
@@ -1,1 +1,24 @@
+//Logical AND (&&)
+let isSwiftFun = true
+let isCodingCool = true
+print(isSwiftFun && isCodingCool)
+
+//Logical OR (||)
+let isSwiftEasy = false
+let isXcodePowerful = true
+print(isSwiftEasy || isXcodePowerful)
+
+//Logical NOT (!)
+let isBugFree = false
+print(!isBugFree)
+
+//Combining Boolean Operators
+let age = 25
+let hasID = true
+let canEnterClub = (age >= 18) && hasID
+print(canEnterClub)
+
+
+
+
 
